6f23d70a1fccc736c0256ebf2aea0aed74f59ea2
shivakumar <shivakumar19d@gmail.com>
2021-11-17 15:47:22 +0530

    cgcs

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ setInterval (function(){
              initialCountDown = initialCountDown  > 0 ? initialCountDown - 1 : 0
              countDownElement.innerHTML=initialCountDown
              var bgcolor = initialCountDown % 2 === 0 ? 'red' : 'purple'
-             Code.body.style.backgroundcolor = bgcolor
+             //Code.body.style.backgroundcolor = bgcolor
+            countDownElement.style.color=bgcolor
 },1000)
