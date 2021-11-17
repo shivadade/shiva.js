45f2f15085240855441f8c0e382da2378aa6aed9
shivakumar <shivakumar19d@gmail.com>
2021-11-17 13:29:11 +0530

    cgcs

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("New JS file")
+
+var countDownElement=document.getElementById("countDown")
+var initialCountDown=countDownElement.innerHTML
+setInterval (function(){ 
+             initialCountDown = initialCountDown  > 0 ? initialCountDown - 1 : 0
+             countDownElement.innerHTML=initialCountDown
+             var bgcolor = initialCountDown % 2 === 0 ? 'red' : 'purple'
+             Code.body.style.backgroundcolor = bgcolor
+},1000)
